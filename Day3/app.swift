6af7bc7ee7806645b8c6477e500d21f233cb66f9
Bradley Black <bradley.black@aviatnet.com>
2019-12-11 18:16:47 +1300

    Day 3: Part 2 Crossed Wires
    Adds the number of steps taken for each wire and find what the shortest
    amount of steps to an intersection is.

--- Day3/app.swift
+++ Day3/app.swift
@@ -2,14 +2,13 @@
 
 import Foundation
 
-func trace_wires(wireArray: [[String]]) -> [[(x: Int, y: Int)]]{
-    var dataToReturn:[[(x: Int, y: Int)]] = []
-    
-    for wire in wireArray{
-        var currentCoordinates = (x: 0, y: 0)
-        var tracedData:[(x: Int, y: Int)] = []
+func trace_wires(wireArray: [[String]]) -> [[(x: Int, y: Int, steps: Int)]]{
+    var dataToReturn:[[(x: Int, y: Int, steps: Int)]] = []
 
-        
+    for wire in wireArray{
+        var currentCoordinates = (x: 0, y: 0, steps: 0)
+        var tracedData:[(x: Int, y: Int, steps: Int)] = []
+
         for direction in wire{
             let index = direction.index(direction.startIndex, offsetBy: 1)
             let change_in_direction = Int(direction[index...])
@@ -17,48 +16,53 @@ func trace_wires(wireArray: [[String]]) -> [[(x: Int, y: Int)]]{
 
                 switch(direction.first){
                 case "L":
-                    currentCoordinates.x -= 1 
+                    currentCoordinates.x -= 1
                     break
                 case "R":
                     currentCoordinates.x += 1
                     break
-                case "U": 
+                case "U":
                     currentCoordinates.y += 1
                     break
                 case "D":
                     currentCoordinates.y -= 1
                     break
-                default: 
+                default:
                     break
                 }
-
-                tracedData.append((x: currentCoordinates.x, y: currentCoordinates.y))
+                currentCoordinates.steps += 1
+                tracedData.append((x: currentCoordinates.x,
+                                   y: currentCoordinates.y,
+                                   steps: currentCoordinates.steps))
             }
         }
         dataToReturn.append(tracedData)
     }
-    return dataToReturn 
+    return dataToReturn
 }
 
-func findClosestIntersection(pathArray: [[(x: Int, y: Int)]]) {
+func findClosestIntersection(pathArray: [[(x: Int, y: Int, steps: Int)]]) {
     var distances:[Int] = []
+    var steps: [Int] = []
     var index = 1
 
     for wire in pathArray{
         for path in wire{
             for otherWire in index..<pathArray.count{
                 for otherPath in pathArray[otherWire]{
-                    if path == otherPath{
-
+                    if path.x == otherPath.x && path.y == otherPath.y{
                         distances.append(abs(path.x) + abs(path.y))
+                        steps.append(path.steps + otherPath.steps)
                     }
                 }
             }
         }
         index += 1
-    } 
+    }
     distances.sort()
+    steps.sort()
     print("The shortest distance is: \(distances[0])")
+    print("The fewest amount of steps is: \(steps[0]))")
 }
 
 func get_input() -> [[String]]{
@@ -68,12 +72,11 @@ func get_input() -> [[String]]{
         let fileContents = try String(contentsOfFile: file_path, encoding: .utf8)
         let tempArray = fileContents.trimmingCharacters(in: .whitespacesAndNewlines)
                                     .components(separatedBy: "\n")
-       
-        
+
         for wire in tempArray{
             wireArray.append(wire.components(separatedBy: ","))
         }
-                                    
+
         return wireArray
     } catch let error as NSError{
         print("Exception: \(error)")
